34fb550be7d64e44d3b5b2249baafc38195af520
Martin Wickham <spexguy070@gmail.com>
2021-11-23 23:16:05 -0600

    More fiddling with identifiers

--- src/day01.zig
+++ src/day01.zig
@@ -2,10 +2,11 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const assert = std.debug.assert;
 const print = std.debug.print;
-const ArrayList = std.ArrayList;
-const HashMap = std.AutoHashMap;
-const StringHashMap = std.StringHashMap;
+const List = std.ArrayList;
+const Map = std.AutoHashMap;
+const StrMap = std.StringHashMap;
 const BitSet = std.DynamicBitSet;
+const Str = []const u8;
 
 const util = @import("util.zig");
 const gpa = util.gpa;
@@ -13,5 +14,5 @@ const gpa = util.gpa;
 const data = @embedFile("../data/day01.txt");
 
 pub fn main() !void {
-    
+
 }
--- src/day15.zig
+++ src/day15.zig
@@ -2,10 +2,11 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const assert = std.debug.assert;
 const print = std.debug.print;
-const ArrayList = std.ArrayList;
-const HashMap = std.AutoHashMap;
-const StringHashMap = std.StringHashMap;
+const List = std.ArrayList;
+const Map = std.AutoHashMap;
+const StrMap = std.StringHashMap;
 const BitSet = std.DynamicBitSet;
+const Str = []const u8;
 
 const util = @import("util.zig");
 const gpa = util.gpa;
