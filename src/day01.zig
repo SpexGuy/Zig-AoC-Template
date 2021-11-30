504784b28eb573ae0368baeb5865befd729c99e4
Martin Wickham <spexguy070@gmail.com>
2021-11-30 17:39:45 -0600

    Add aliases for useful std lib functions

--- src/day01.zig
+++ src/day01.zig
@@ -1,7 +1,5 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
-const assert = std.debug.assert;
-const print = std.debug.print;
 const List = std.ArrayList;
 const Map = std.AutoHashMap;
 const StrMap = std.StringHashMap;
@@ -16,3 +14,28 @@ const data = @embedFile("../data/day01.txt");
 pub fn main() !void {
 
 }
+
+// Useful stdlib functions
+const tokenize = std.mem.tokenize;
+const split = std.mem.split;
+const indexOf = std.mem.indexOfScalar;
+const indexOfAny = std.mem.indexOfAny;
+const indexOfStr = std.mem.indexOfPosLinear;
+const lastIndexOf = std.mem.lastIndexOfScalar;
+const lastIndexOfAny = std.mem.lastIndexOfAny;
+const lastIndexOfStr = std.mem.lastIndexOfLinear;
+const trim = std.mem.trim;
+const sliceMin = std.mem.min;
+const sliceMax = std.mem.max;
+
+const min = std.math.min;
+const min3 = std.math.min3;
+const max = std.math.max;
+const max3 = std.math.max3;
+
+const print = std.debug.print;
+const assert = std.debug.assert;
+
+const sort = std.sort.sort;
+const asc = std.sort.asc;
+const desc = std.sort.desc;
--- src/util.zig
+++ src/util.zig
@@ -1,7 +1,5 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
-const assert = std.debug.assert;
-const print = std.debug.print;
 const List = std.ArrayList;
 const Map = std.AutoHashMap;
 const StrMap = std.StringHashMap;
@@ -12,3 +10,29 @@ var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
 pub const gpa = &gpa_impl.allocator;
 
 // Add utility functions here
+
+
+// Useful stdlib functions
+const tokenize = std.mem.tokenize;
+const split = std.mem.split;
+const indexOf = std.mem.indexOfScalar;
+const indexOfAny = std.mem.indexOfAny;
+const indexOfStr = std.mem.indexOfPosLinear;
+const lastIndexOf = std.mem.lastIndexOfScalar;
+const lastIndexOfAny = std.mem.lastIndexOfAny;
+const lastIndexOfStr = std.mem.lastIndexOfLinear;
+const trim = std.mem.trim;
+const sliceMin = std.mem.min;
+const sliceMax = std.mem.max;
+
+const min = std.math.min;
+const min3 = std.math.min3;
+const max = std.math.max;
+const max3 = std.math.max3;
+
+const print = std.debug.print;
+const assert = std.debug.assert;
+
+const sort = std.sort.sort;
+const asc = std.sort.asc;
+const desc = std.sort.desc;
