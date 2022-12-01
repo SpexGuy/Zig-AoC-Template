ee867261420251d919753930998e4e940b4f1279
Chris Riccomini <criccomini@apache.org>
2022-12-01 09:48:48 -0800

    Solved day 1

--- src/day01.zig
+++ src/day01.zig
@@ -11,7 +11,25 @@ const gpa = util.gpa;
 const data = @embedFile("data/day01.txt");
 
 pub fn main() !void {
-    
+  // Trim trailing \n's in file
+  var trimmed_data = trim(u8, data, "\n");
+  // Get each elf's calories as a separate string
+  var elf_splits = split(u8, trimmed_data, "\n\n");
+  var elf_calories = List(u32).init(std.heap.page_allocator);
+  while (elf_splits.next()) |elf_split| {
+    // Get each calorie line and count them up
+    var foodItems = split(u8, elf_split, "\n");
+    var totalCaloriesForElf: u32 = 0;
+    while (foodItems.next()) |caloriesStr| {
+      totalCaloriesForElf += try parseInt(u32, caloriesStr, 10);
+    }
+    try elf_calories.append(totalCaloriesForElf);
+  }
+  var sorted_calories = elf_calories.items;
+  std.sort.sort(u32, sorted_calories, {}, comptime std.sort.desc(u32));
+  print("Answer 1: {d}\n", .{sorted_calories[0]});
+  var top_3_calories = sorted_calories[0] + sorted_calories[1] + sorted_calories[2];
+  print("Answer 2: {d}\n", .{top_3_calories});
 }
 
 // Useful stdlib functions
@@ -41,7 +59,3 @@ const assert = std.debug.assert;
 const sort = std.sort.sort;
 const asc = std.sort.asc;
 const desc = std.sort.desc;
-
-// Generated from template/template.zig.
-// Run `zig build generate` to update.
-// Only unmodified days will be updated.
