b1584f91a242db35e7c3f730b780b3c83060eacf
Krzysztof Wolicki <der.teufel.mail@gmail.com>
2023-11-29 08:06:07 +0100

    Update template to latest master

--- src/day14.zig
+++ src/day14.zig
@@ -10,13 +10,15 @@ const gpa = util.gpa;
 
 const data = @embedFile("data/day14.txt");
 
-pub fn main() !void {
-    
-}
+pub fn main() !void {}
 
 // Useful stdlib functions
-const tokenize = std.mem.tokenize;
-const split = std.mem.split;
+const tokenizeAny = std.mem.tokenizeAny;
+const tokenizeSeq = std.mem.tokenizeSequence;
+const tokenizeSca = std.mem.tokenizeScalar;
+const splitAny = std.mem.splitAny;
+const splitSeq = std.mem.splitSequence;
+const splitSca = std.mem.splitScalar;
 const indexOf = std.mem.indexOfScalar;
 const indexOfAny = std.mem.indexOfAny;
 const indexOfStr = std.mem.indexOfPosLinear;
@@ -30,15 +32,10 @@ const sliceMax = std.mem.max;
 const parseInt = std.fmt.parseInt;
 const parseFloat = std.fmt.parseFloat;
 
-const min = std.math.min;
-const min3 = std.math.min3;
-const max = std.math.max;
-const max3 = std.math.max3;
-
 const print = std.debug.print;
 const assert = std.debug.assert;
 
-const sort = std.sort.sort;
+const sort = std.sort.block;
 const asc = std.sort.asc;
 const desc = std.sort.desc;
 
