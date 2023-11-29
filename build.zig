8208daaf7a5b8e8e112346eff1e13386e4ff1d5b
Krzysztof Wolicki <der.teufel.mail@gmail.com>
2023-11-29 08:06:07 +0100

    Update template to latest master

--- build.zig
+++ build.zig
@@ -1,61 +1,35 @@
 const std = @import("std");
-const Builder = std.build.Builder;
-const LibExeObjStep = std.build.LibExeObjStep;
-
-const required_zig_version = std.SemanticVersion.parse("0.10.0") catch unreachable;
-const padded_int_fix = std.SemanticVersion.parse("0.11.0-dev.331+304e82808") catch unreachable;
+const Build = std.Build;
+const CompileStep = std.Build.Step.Compile;
 
 /// set this to true to link libc
 const should_link_libc = false;
 
-fn linkObject(b: *Builder, obj: *LibExeObjStep) void {
+fn linkObject(b: *Build, obj: *CompileStep) void {
     if (should_link_libc) obj.linkLibC();
     _ = b;
-
-    // Padded integers are buggy in 0.10.0, fixed in 0.11.0-dev.331+304e82808
-    // This is especially bad for AoC because std.StaticBitSet is commonly used.
-    // If your version is older than that, we use stage1 to avoid this bug.
-    // Issue: https://github.com/ziglang/zig/issues/13480
-    // Fix: https://github.com/ziglang/zig/pull/13637
-    if (comptime @import("builtin").zig_version.order(padded_int_fix) == .lt) {
-        obj.use_stage1 = true;
-    }
 
     // Add linking for packages or third party libraries here
 }
 
-pub fn build(b: *Builder) void {
-    if (comptime @import("builtin").zig_version.order(required_zig_version) == .lt) {
-        std.debug.print(
-            \\Error: Your version of Zig is missing features that are needed for this template.
-            \\You will need to download a newer build.
-            \\
-            \\    https://ziglang.org/download/
-            \\
-            \\
-        , .{});
-        std.os.exit(1);
-    }
-
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
+pub fn build(b: *Build) void {
     const target = b.standardTargetOptions(.{});
-
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const mode = b.standardOptimizeOption(.{});
 
     const install_all = b.step("install_all", "Install all days");
     const install_all_tests = b.step("install_tests_all", "Install tests for all days");
+    _ = install_all_tests;
     const run_all = b.step("run_all", "Run all days");
 
     const generate = b.step("generate", "Generate stub files from template/template.zig");
-    const build_generate = b.addExecutable("generate", "template/generate.zig");
-    build_generate.setBuildMode(.ReleaseSafe);
-    const run_generate = build_generate.run();
-    run_generate.cwd = std.fs.path.dirname(@src().file).?;
+    const build_generate = b.addExecutable(.{
+        .name = "generate",
+        .root_source_file = .{ .path = "template/generate.zig" },
+        .optimize = .ReleaseSafe,
+    });
+
+    const run_generate = b.addRunArtifact(build_generate);
+    run_generate.setCwd(.{ .path = std.fs.path.dirname(@src().file).? });
     generate.dependOn(&run_generate.step);
 
     // Set up an exe for each day
@@ -64,24 +38,31 @@ pub fn build(b: *Builder) void {
         const dayString = b.fmt("day{:0>2}", .{day});
         const zigFile = b.fmt("src/{s}.zig", .{dayString});
 
-        const exe = b.addExecutable(dayString, zigFile);
-        exe.setTarget(target);
-        exe.setBuildMode(mode);
+        const exe = b.addExecutable(.{
+            .name = dayString,
+            .root_source_file = .{ .path = zigFile },
+            .target = target,
+            .optimize = mode,
+        });
         linkObject(b, exe);
-        exe.install();
+        // b.installArtifact(exe);
 
-        const install_cmd = b.addInstallArtifact(exe);
+        const install_cmd = b.addInstallArtifact(exe, .{});
 
-        const run_test = b.addTest(zigFile);
-        run_test.setTarget(target);
-        run_test.setBuildMode(mode);
-        linkObject(b, exe);
+        const build_test = b.addTest(.{
+            .root_source_file = .{ .path = zigFile },
+            .target = target,
+            .optimize = mode,
+        });
+        linkObject(b, build_test);
 
-        const build_test = b.addTestExe(b.fmt("test_{s}", .{dayString}), zigFile);
-        build_test.setTarget(target);
-        build_test.setBuildMode(mode);
-        linkObject(b, exe);
-        const install_test = b.addInstallArtifact(build_test);
+        const run_test = b.addRunArtifact(build_test);
+
+        // const build_test = b.addTestExe(b.fmt("test_{s}", .{dayString}), zigFile);
+        // build_test.setTarget(target);
+        // build_test.setBuildMode(mode);
+        // linkObject(b, exe);
+        // const install_test = b.addInstallArtifact(build_test);
 
         {
             const step_key = b.fmt("install_{s}", .{dayString});
@@ -98,16 +79,7 @@ pub fn build(b: *Builder) void {
             step.dependOn(&run_test.step);
         }
 
-        {
-            const step_key = b.fmt("install_tests_{s}", .{dayString});
-            const step_desc = b.fmt("Install test_{s}.exe", .{dayString});
-            const step = b.step(step_key, step_desc);
-            step.dependOn(&install_test.step);
-            install_all_tests.dependOn(&install_test.step);
-        }
-
-        const run_cmd = exe.run();
-        run_cmd.step.dependOn(&install_cmd.step);
+        const run_cmd = b.addRunArtifact(exe);
         if (b.args) |args| {
             run_cmd.addArgs(args);
         }
@@ -121,42 +93,12 @@ pub fn build(b: *Builder) void {
     // Set up tests for util.zig
     {
         const test_util = b.step("test_util", "Run tests in util.zig");
-        const test_cmd = b.addTest("src/util.zig");
-        test_cmd.setTarget(target);
-        test_cmd.setBuildMode(mode);
+        const test_cmd = b.addTest(.{
+            .root_source_file = .{ .path = "src/util.zig" },
+            .target = target,
+            .optimize = mode,
+        });
         linkObject(b, test_cmd);
         test_util.dependOn(&test_cmd.step);
-    }
-
-    // Set up test executable for util.zig
-    {
-        const test_util = b.step("install_tests_util", "Run tests in util.zig");
-        const test_exe = b.addTestExe("test_util", "src/util.zig");
-        test_exe.setTarget(target);
-        test_exe.setBuildMode(mode);
-        linkObject(b, test_exe);
-        const install = b.addInstallArtifact(test_exe);
-        test_util.dependOn(&install.step);
-    }
-
-    // Set up a step to run all tests
-    {
-        const test_step = b.step("test", "Run all tests");
-        const test_cmd = b.addTest("src/test_all.zig");
-        test_cmd.setTarget(target);
-        test_cmd.setBuildMode(mode);
-        linkObject(b, test_cmd);
-        test_step.dependOn(&test_cmd.step);
-    }
-
-    // Set up a step to build tests (but not run them)
-    {
-        const test_build = b.step("install_tests", "Install test_all.exe");
-        const test_exe = b.addTestExe("test_all", "src/test_all.zig");
-        test_exe.setTarget(target);
-        test_exe.setBuildMode(mode);
-        linkObject(b, test_exe);
-        const test_exe_install = b.addInstallArtifact(test_exe);
-        test_build.dependOn(&test_exe_install.step);
     }
 }
--- template/generate.zig
+++ template/generate.zig
@@ -15,7 +15,7 @@ fn instantiateTemplate(template: []const u8, day: u32) ![]const u8 {
     while (std.mem.indexOfScalar(u8, rest, '$')) |index| {
         try list.appendSlice(rest[0..index]);
         try std.fmt.format(list.writer(), "{d:0>2}", .{day});
-        rest = rest[index+1..];
+        rest = rest[index + 1 ..];
     }
     try list.appendSlice(rest);
     return list.toOwnedSlice();
@@ -31,7 +31,7 @@ fn readHashes() !*[25][Hash.digest_length]u8 {
     if (hash_bytes.len != 25 * Hash.digest_length)
         return error.InvalidFormat;
 
-    return @ptrCast(*[25][Hash.digest_length]u8, hash_bytes.ptr);
+    return @ptrCast(hash_bytes.ptr);
 }
 
 pub fn main() !void {
@@ -41,7 +41,7 @@ pub fn main() !void {
         error.FileNotFound => blk: {
             std.debug.print("{s} doesn't exist, will assume all files have been modified.\nDelete src/dayXX.zig and rerun `zig build generate` to regenerate it.\n", .{hashes_file});
             const mem = try gpa.allocator().create([25][Hash.digest_length]u8);
-            @memset(@ptrCast([*]u8, mem), 0, @sizeOf(@TypeOf(mem.*)));
+            @memset(std.mem.sliceAsBytes(mem), 0);
             break :blk mem;
         },
         error.InvalidFormat => {
@@ -49,7 +49,7 @@ pub fn main() !void {
             std.os.exit(1);
         },
         else => |e| {
-            std.debug.print("Failed to open {s}: {}\n", .{hashes_file, e});
+            std.debug.print("Failed to open {s}: {}\n", .{ hashes_file, e });
             return e;
         },
     };
@@ -62,7 +62,7 @@ pub fn main() !void {
         defer gpa.allocator().free(filename);
 
         var new_file = false;
-        const file = std.fs.cwd().openFile(filename, .{.mode = .read_write}) catch |err| switch (err) {
+        const file = std.fs.cwd().openFile(filename, .{ .mode = .read_write }) catch |err| switch (err) {
             error.FileNotFound => blk: {
                 new_file = true;
                 break :blk try std.fs.cwd().createFile(filename, .{});
@@ -86,7 +86,7 @@ pub fn main() !void {
             var hash: [Hash.digest_length]u8 = undefined;
             Hash.hash(contents, &hash, .{});
 
-            regenerate = std.mem.eql(u8, &hash, &hashes[day-1]);
+            regenerate = std.mem.eql(u8, &hash, &hashes[day - 1]);
         } else {
             regenerate = true;
         }
@@ -100,7 +100,7 @@ pub fn main() !void {
             const text = try instantiateTemplate(template, day);
             defer gpa.allocator().free(text);
 
-            Hash.hash(text, &hashes[day-1], .{});
+            Hash.hash(text, &hashes[day - 1], .{});
             updated_hashes = true;
 
             try file.writeAll(text);
@@ -118,7 +118,7 @@ pub fn main() !void {
     if (updated_hashes) {
         try std.fs.cwd().writeFile(hashes_file, std.mem.asBytes(hashes));
         if (skipped_any) {
-            std.debug.print("Some days were skipped. Delete them to force regeneration.\n",.{});
+            std.debug.print("Some days were skipped. Delete them to force regeneration.\n", .{});
         }
     } else {
         std.debug.print("No updates made, all days were modified. Delete src/dayXX.zig to force regeneration.\n", .{});
