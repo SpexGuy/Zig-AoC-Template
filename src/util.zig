7cee78145f0dd98b6f41cdee79647d4a2119fe17
Krzysztof Wolicki <der.teufel.mail@gmail.com>
2023-12-01 05:27:31 +0100

    Update to latest master (#5)
    * Update template to latest master
    
    * Update README.md
    
    * Add build.zig.zon
    
    * Add required_zig_version back
    
    * Remove unused step

--- src/util.zig
+++ src/util.zig
@@ -11,10 +11,13 @@ pub const gpa = gpa_impl.allocator();
 
 // Add utility functions here
 
-
 // Useful stdlib functions
-const tokenize = std.mem.tokenize;
-const split = std.mem.split;
+const tokenizeAny = std.mem.tokenizeAny;
+const tokenizeSeq = std.mem.tokenizeSequence;
+const tokenizeSca = std.mem.tokenizeScalar;
+const splitAny = std.mem.splitAny;
+const splitSeq = std.mem.splitSequence;
+const splitSca = std.mem.splitScalar;
 const indexOf = std.mem.indexOfScalar;
 const indexOfAny = std.mem.indexOfAny;
 const indexOfStr = std.mem.indexOfPosLinear;
@@ -28,14 +31,9 @@ const sliceMax = std.mem.max;
 const parseInt = std.fmt.parseInt;
 const parseFloat = std.fmt.parseFloat;
 
-const min = std.math.min;
-const min3 = std.math.min3;
-const max = std.math.max;
-const max3 = std.math.max3;
-
 const print = std.debug.print;
 const assert = std.debug.assert;
 
-const sort = std.sort.sort;
+const sort = std.sort.block;
 const asc = std.sort.asc;
 const desc = std.sort.desc;
